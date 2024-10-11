1b38fab50c6d8698e9a5e26eebb92c07ee4dbe14
andydangit <andy.dang.it@gmail.com>
2024-10-11 09:26:48 -0500

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Andy Dang It on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
