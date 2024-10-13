ebb465a1a637c40d9a372dd3fa5c70ae55b29641
andydangit <andy.dang.it@gmail.com>
2024-10-13 12:42:55 -0500

    Add: order of animation

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -9,32 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     
     var body: some View {
-//        print(animationAmount)
-//        
-//        return VStack {
-//            Stepper("Scale amount", value: $animationAmount.animation(
-//                .easeInOut(duration: 1)
-//                .repeatCount(3, autoreverses: true)
-//            ), in: 1...10)
-//            Spacer()
-            
-        
+                
             Button("Tap me") {
-                withAnimation(.spring(duration: 1, bounce: 0.5)){
-                    animationAmount += 360
+                enabled.toggle()
                 }
-            }
-            .padding(40)
-            .background(Color.red)
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
             .foregroundStyle(.white)
-            .clipShape(.circle)
-            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+            .animation(.default, value: enabled)
+            .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+            .animation(.spring(duration: 1, bounce: 0.7), value: enabled)
+            
+            
+}
+
    
-        }
+        
         
     
     
