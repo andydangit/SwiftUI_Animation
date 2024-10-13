cb5a8aa9a814e68e3fd558bc54737778511c01af
andydangit <andy.dang.it@gmail.com>
2024-10-13 13:08:29 -0500

    Add: Gesture animation to move the larray letter

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -10,33 +10,45 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var dragAmount = CGSize.zero
+    @State private var enabled = false
+    
+    let letters = Array("Hello SwiftUI ")
     
     
     var body: some View {
-       
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation}
-                    .onEnded { _ in
-                        withAnimation(.bouncy) {
-                            dragAmount = .zero}
-                    }
-            )
-
+        
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) {
+                num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red )
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+            }
         }
-            
-
-
-   
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation}
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
         
         
+        
+    }
     
     
-//MARK: - End of View
+    
+    
+    
+    
+    
+    
+    //MARK: - End of View
 }
 
 #Preview {
